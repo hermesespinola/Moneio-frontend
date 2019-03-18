6c685e2a383010fe5bd8258bd59a0d321407f761
rosamariaramirez <rosa.centauri@gmail.com>
2019-03-17 23:24:59 -0600

    fix for submit form
    Changed cords object for latitud an longitud

--- moneio/src/api.js
+++ moneio/src/api.js
@@ -1,15 +1,27 @@
-const apiURL = process.env.API_URL
+const apiURL = process.env.API_URL ? process.env.API_URL : "http://127.0.0.1:8080";
 console.log('API_URL:', apiURL);
 
 // TODO: include denomination
-export const submitForm = (serialCode, notes, coords, image, denomination) => {
-  const formData = new FormData()
-  formData.append('serialCode', serialCode)
-  formData.append('denomination', denomination)
-  if (notes && typeof notes === 'string') formData.append('notes', notes)
-  if (coords && typeof coords === 'object') formData.append('coords', coords)
-  if (image && typeof image === 'object') formData.append('image', image)
+export const submitForm = (serialCode, notes, latitude, longitude, image, denomination) => {
+  console.log("SERIAL CODE AND DENOMINATION")
+  console.log(serialCode)
+  console.log(denomination)
 
+  var formData = new FormData()
+  formData.append('serialCode', serialCode)
+  formData.append('denomination', 20)
+  if (notes && typeof notes === 'string') formData.append('notes', notes)
+  if (latitude && typeof latitude === 'number') formData.append('latitude', latitude)
+  if (longitude && typeof longitude === 'number') formData.append('longitude', longitude)
+  if (image && typeof image === 'object') formData.append('image', image)
+  for (var key of formData.keys()) {
+    console.log(key); 
+  }
+  for (var value of formData.values()) {
+    console.log(value); 
+  }
+  //foreach (let entry : formData.entries()) {
+ 
   if (!apiURL) {
     // Mock some data
     return Promise.resolve({
@@ -20,7 +32,6 @@ export const submitForm = (serialCode, notes, coords, image, denomination) => {
 
   return fetch(`${apiURL}/upload-bill`, {
     method: 'POST',
-    headers: { 'Content-Type': 'multipart/form-data' },
     body: formData,
   })
 }
@@ -29,10 +40,8 @@ export const fetchExplore = async (option) => {
   if (!apiURL) {
     return [
       {
-        coords: {
-          latitude: 52.496912,
-          longitude: 13.436738,
-        },
+        latitude: 52.496912,
+        longitude: 13.436738,
         date: 'Sun Feb 10 2019 17:18:06',
         image: 'https://static.vix.com/es/sites/default/files/styles/large/public/btg/curiosidades.batanga.com/files/Que-figuras-historicas-estan-en-los-billetes-de-Mexico-01.png?itok=kGQNOmbs',
         notes: 'Este es es benito, sus amiguitos son María, Nezahualcóyotl, Sor Juana, el buen Diego Rivera y Frida Kalho, y como olvidar a Miguel Hidalgo.',
@@ -42,7 +51,6 @@ export const fetchExplore = async (option) => {
   }
   return await fetch(`${apiURL}/explore/${option}`, {
     method: 'GET',
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
   })
   .then(res => res.json())
 }
@@ -51,10 +59,8 @@ export const fetchEntries = async (serialCode) => {
   if (!apiURL) {
     return [
       {
-        coords: {
-          latitude: 52.496912,
-          longitude: 13.436738,
-        },
+        latitude: 52.496912,
+        longitude: 13.436738,
         date: 'Sun Feb 10 2019 17:18:06',
         image: 'https://static.vix.com/es/sites/default/files/styles/large/public/btg/curiosidades.batanga.com/files/Que-figuras-historicas-estan-en-los-billetes-de-Mexico-01.png?itok=kGQNOmbs',
         notes: 'Este es es benito, sus amiguitos son María, Nezahualcóyotl, Sor Juana, el buen Diego Rivera y Frida Kalho, y como olvidar a Miguel Hidalgo.',
@@ -64,7 +70,6 @@ export const fetchEntries = async (serialCode) => {
   }
   return await fetch(`${apiURL}/entries/${serialCode}`, {
     method: 'GET',
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
   })
   .then(res => res.json())
 }
